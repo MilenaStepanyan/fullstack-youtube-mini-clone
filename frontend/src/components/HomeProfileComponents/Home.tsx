58992b021adfb702677889bd45713800bbe5ec8d
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-08-23 00:53:50 +0400

    css writted for everything

--- frontend/src/components/HomeProfileComponents/Home.tsx
+++ frontend/src/components/HomeProfileComponents/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import YouTube_logo from "../../public/YouTube_logo.png";
+import YouTube_logo from "../../../public/YouTube_logo.png";
 import { Search } from "../techComponents/Search";
 import { Link } from "react-router-dom";
 
