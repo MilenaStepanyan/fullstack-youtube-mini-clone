f258ac31cd96e61b082ef45734e23de84f6256c2
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-08-22 18:31:20 +0400

    uploading videos handled

--- backend/src/controller/videoController.ts
+++ backend/src/controller/videoController.ts
@@ -30,3 +30,37 @@ export const getVideos = async (
     return res.status(500).json({ msg: "Server error" });
   }
 };
+export const uploadVideo = (req: Request, res: Response): void => {
+  upload.single("video")(req, res, async (err: any) => {
+    if (err) {
+      console.error("Upload Error:", err);
+      return res.status(400).json({ msg: err.message });
+    }
+
+    if (!req.file) {
+      return res.status(400).json({ msg: "No file uploaded" });
+    }
+    const userId = req.body.userId; 
+    const title = req.body.title
+    const description = req.body.description;
+    const filename = req.file?.filename || '';
+    const mimetype = req.file?.mimetype || '';
+    const size = req.file?.size || 0;
+    const thumbnail = ''; 
+    const query = `
+    INSERT INTO videos (user_id, title, description, filename, mimetype, size, thumbnail, created_at) 
+    VALUES (?, ?, ?, ?, ?, ?, ?, NOW())
+  `;
+  if (!userId || !title) {
+    return res.status(400).json({ msg: "User ID and title are required" });
+  }
+
+  try{
+    await pool.query(query,[userId,title,description,filename,mimetype,size,thumbnail])
+    res.status(200).json({msg:"Video uploaded successfully", file:req.file})
+  }catch(error){
+    log(error)
+     res.status(500).json({msg:"Server error"})
+  }
+  });
+};
