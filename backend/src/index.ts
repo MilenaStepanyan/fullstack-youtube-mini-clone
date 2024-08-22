683104e912eeb879313e32cece2a7850ac52663b
imnotmilena <stepanyanmil7.ait@gmail.com>
2024-08-22 17:21:27 +0400

    typescript setted

---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+import express from "express"
+//import cors from "cors"
+//import { config } from 'dotenv';
+
+const app = express();
+// config();
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     allowedHeaders: "Content-Type, Authorization",
+//   })
+// );
+app.use(express.json());
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from the backend!' });
+});
+
+const PORT =  3009;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
